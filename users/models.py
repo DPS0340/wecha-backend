7c3eda5fc8df3809a972c820d005daf28557dd7a
Choonghee Lee <choonghee.dev@google.com>
2020-08-19 13:31:14 +0900

    add: users app models 추가
    - 유저 관련 모델 클래스 4개 추가
    - 필요없는 코드 주석 제거

--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from films.models import Film
+
+class User(models.Model):
+    email          = models.EmailField(max_length=256)
+    password       = models.CharField(max_length=512)
+    name           = models.CharField(max_length=128)
+    description    = models.TextField(null=True)
+    face_url       = models.URLField(max_length=2048, null=True)
+    minute_watched = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "users"
+
+class Collection(models.Model):
+    name = models.CharField(max_length=512)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    film = models.ManyToManyField(Film)
+
+    class Meta:
+        db_table = "collections"
+        
+class ReviewType(models.Model):
+    name = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = "review_types"
+
+class Review(models.Model):
+    score       = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+    comment     = models.TextField(null=True)
+    like_count  = models.IntegerField(default=0)
+    review_type = models.ForeignKey(ReviewType, on_delete=models.CASCADE)
+    film        = models.ForeignKey(Film, on_delete=models.CASCADE)
+    user        = models.ManyToManyField(User)
+
+    class Meta:
+        db_table = "reviews"
