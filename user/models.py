03c0bda9345cc69a37b2e1ebfa91fa14e43c7a82
Choonghee Lee <choonghee.dev@google.com>
2020-08-20 15:23:19 +0900

    Modify: 유저 앱 피드백 반영
    - 다대다 중간 테이블 클래스 생성
    - TYPE_CHOICES 삭제
    - 코드 정렬

--- user/models.py
+++ user/models.py
@@ -15,30 +15,43 @@ class User(models.Model):
 
 class Collection(models.Model):
     name = models.CharField(max_length=512)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    film = models.ManyToManyField(Film)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    film = models.ManyToManyField('Film', through="FilmCollection")
 
     class Meta:
         db_table = "collections"
+
+class FilmCollection(models.Model): 
+    film       = models.ForeignKey('Film' on_delete=models.CASCADE)
+    collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "film_collections"
         
 class ReviewType(models.Model):
-    TYPE_CHOICES = (
-        ('R', 'Rated'),
-        ('W', 'Wish'),
-        ('O', 'Ongoing'),
-    )
-    name = models.CharField(max_length=128, choices=TYPE_CHOICES)
+    name = models.CharField(max_length=128)
 
     class Meta:
         db_table = "review_types"
 
 class Review(models.Model):
-    score       = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+    score       = models.DecimalField(
+        max_digits     = 2,
+        decimal_places = 1,
+        null           = True
+    )
     comment     = models.TextField(null=True)
     like_count  = models.IntegerField(default=0)
-    review_type = models.ForeignKey(ReviewType, on_delete=models.CASCADE)
-    film        = models.ForeignKey(Film, on_delete=models.CASCADE)
-    user        = models.ManyToManyField(User)
+    review_type = models.ForeignKey('ReviewType', on_delete=models.CASCADE)
+    film        = models.ForeignKey('Film', on_delete=models.CASCADE)
+    user        = models.ManyToManyField('User')
 
     class Meta:
         db_table = "reviews"
+
+class UserReview(models.Model):
+    user   = models.ForeignKey('User', on_delete=models.CASCADE)
+    review = models.ForeignKey('Review', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user_reviews"
