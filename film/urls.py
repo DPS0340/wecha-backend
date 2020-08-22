f594c8ad5e8791fc314df67afe9ee07384b62b2d
Choonghee Lee <choonghee.dev@google.com>
2020-08-22 20:25:56 +0900

    Modify: FilmRankingView로 클래스 이름 변경

--- film/urls.py
+++ film/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from .views import (
-    FilmRankingByServiceProvierView,
+    FilmRankingView,
     FilmDetailView
 )
 
 app_name = 'film'
 
 urlpatterns = [
-    path('ranking', FilmRankingByServiceProvierView.as_view(), name="ranking"),
+    path('ranking', FilmRankingView.as_view(), name="ranking"),
     path('<int:film_id>', FilmDetailView.as_view(), name="detail"),
 ]
